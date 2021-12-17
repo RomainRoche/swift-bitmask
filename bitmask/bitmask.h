d56aa7b68e4afbb5617877d4b86b45a93b64acc7
Romain Roche <romain@wildwinewest.com>
2021-12-17 09:41:31 +0100

    Initial Commit

---
+++ bitmask/bitmask.h
@@ -0,0 +1,18 @@
+//
+//  bitmask.h
+//  bitmask
+//
+//  Created by Romain Roche on 17/12/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for bitmask.
+FOUNDATION_EXPORT double bitmaskVersionNumber;
+
+//! Project version string for bitmask.
+FOUNDATION_EXPORT const unsigned char bitmaskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <bitmask/PublicHeader.h>
+
+
