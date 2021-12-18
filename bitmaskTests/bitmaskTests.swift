28c68732b09d0092190469c080f102126b74a339
Romain Roche <romain.roche.perso@gmail.com>
2021-12-18 10:33:07 +0100

    chg: unit tests WIP

--- bitmaskTests/bitmaskTests.swift
+++ bitmaskTests/bitmaskTests.swift
@@ -6,10 +6,19 @@
 //
 
 import XCTest
-@testable import bitmask
+@testable import Bitmask
 
 class bitmaskTests: XCTestCase {
 
+    enum TestEnum: Int, Maskable {
+        case none   = 0b00000000
+        case first  = 0b00000001
+        case second = 0b00000010
+        case third  = 0b00000100
+        case fourth = 0b00001000
+        case fifth  = 0b00010000
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,19 +27,21 @@ class bitmaskTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_bitmask_init() throws {
+        let bitmask = Bitmask(with: TestEnum.second)
+        XCTAssert(bitmask.rawValue == 2)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_bitmask_and() throws {
+        var bitmask = Bitmask(with: TestEnum.first)
+        bitmask = bitmask & .second
+        XCTAssert(bitmask.rawValue == 0)
+    }
+    
+    func test_bitmask_or() throws {
+        var bitmask = Bitmask(with: TestEnum.first)
+        bitmask = bitmask | .second
+        XCTAssert(bitmask.rawValue == 3)
     }
 
 }
